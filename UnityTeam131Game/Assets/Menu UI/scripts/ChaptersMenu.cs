6c6e17382ba0ae9c6046dccd37e4bee5126bcebb
nenavc <nur.senavci@metu.edu.tr>
2022-06-04 17:19:53 +0300

    MainMenu
    Main, options ve chapters menülerini ekledim

---
+++ UnityTeam131Game/Assets/Menu UI/scripts/ChaptersMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChaptersMenu : MonoBehaviour
+{
+ public void PlayChapterOne()
+ {
+  SceneManager.LoadScene(1);
+ }
+}
---
+++ UnityTeam131Game/Assets/Menu UI/scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+public void QuitGame ()
+{
+    Debug.Log("QUIT!");
+Application.Quit();
+}
+}
